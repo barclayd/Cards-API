7f5b363683f8b96f145e93ff8e681c466027916a
Daniel Barclay <barclaysd@me.com>
2020-11-21 19:07:21 +0000

    .env typescript support and scripts to generate client schema and setupDocker

---
+++ src/env.d.ts
@@ -0,0 +1,10 @@
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      NODE_ENV: 'development' | 'production' | 'ci' | 'test';
+      DATABASE_URL: string;
+    }
+  }
+}
+
+export {}
