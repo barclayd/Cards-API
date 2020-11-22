96335f4fc3627b870f6f07519ba89ebf4d0ae41c
Daniel Barclay <barclaysd@me.com>
2020-11-22 12:06:14 +0000

    NetworkService.spec.ts

--- src/services/NetworkService.ts
+++ src/services/NetworkService.ts
@@ -6,19 +6,12 @@ import { QueryError, ErrorMessage } from '@/helpers/error';
 export class NetworkService implements INetwork {
   constructor(public baseURL: string) {}
 
-  public async get<T>(
-    endpoint: Endpoint,
-    errorMessage?: ErrorMessage,
-  ): Promise<T> {
-    try {
-      const URL = `${this.baseURL}/${endpoint}.json`;
-      const response = await axios.get(URL);
-      if (response.data) {
-        return response.data;
-      }
-      throw errorMessage;
-    } catch (error) {
-      throw new QueryError(errorMessage ?? ErrorMessage.generic);
+  public async get<T>(endpoint: Endpoint): Promise<T> {
+    const URL = `${this.baseURL}/${endpoint}.json`;
+    const response = await axios.get(URL);
+    if (response.data) {
+      return response.data;
     }
+    throw new QueryError(`${ErrorMessage.generic} for endpoint: ${endpoint}`);
   }
 }
