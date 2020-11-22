f56f26a95c3d3211ab320348d9484c93528ce9be
Daniel Barclay <barclaysd@me.com>
2020-11-22 20:02:49 +0000

    Refactor ErrorMessage and QueryError into models and entity respectively

---
+++ src/models/ErrorMessage.ts
@@ -0,0 +1,6 @@
+export enum ErrorMessage {
+  generic = 'Failed to retrieve query',
+  cardPreviewGeneration = 'Unable to generate card for the options provided',
+  missingTemplateForCard = 'No template is available for the card selected',
+  missingPriceInformationForSize = 'No price information is available for the card size selected',
+}
