5d3b33c832208c40c58d7d1d3251694ef70358ec
Daniel Barclay <barclaysd@me.com>
2020-11-23 15:15:46 +0000

    CardsResolver - card query integration tests

--- tests/integration/queries/CardsResolver.spec.ts
+++ tests/integration/queries/CardsResolver.spec.ts
@@ -1,6 +1,10 @@
 import { ExecutionResult } from 'graphql';
 import { setupGraphQL } from '@t/helpers/setupGraphQL';
-import { CARDS_QUERY } from '@t/graphql/queries/cards';
+import { CARD_QUERY, CARDS_QUERY } from '@t/graphql/queries/cards';
+import { CardQueryInput } from '@t/models/CardQueryInput';
+import { SizeOption } from '@/models/ISize';
+import { QueryError } from '@/entity/QueryError';
+import { ErrorMessage } from '@/models/ErrorMessage';
 
 describe('CardsResolver - cards query', () => {
   let query: ExecutionResult;
@@ -41,9 +45,90 @@ describe('CardsResolver - cards query', () => {
 });
 
 describe('CardsResolver - card query', () => {
-  it("returns the correct data for cardId of 'card001' and size: 'gt'", () => {});
-  it('returns a card with the correct size when size passed as an argument', () => {});
-  it('returns a card with the correct size as null when size is not passed as an argument', () => {});
-  it('returns a card with the correct price when size is passed as an argument', () => {});
-  it('returns a card with the correct price when size is not passed as an argument', () => {});
+  const cardQuery = async (variables: CardQueryInput) => {
+    return await setupGraphQL({
+      source: CARD_QUERY,
+      variableValues: variables,
+    });
+  };
+
+  it("returns the correct data for cardId of 'card001' and size: 'gt'", async () => {
+    const expectedCard = {
+      title: 'card 1 title',
+      size: 'gt',
+      availableSizes: [
+        {
+          id: 'sm',
+          title: 'Small',
+        },
+        {
+          id: 'md',
+          title: 'Medium',
+        },
+        {
+          id: 'gt',
+          title: 'Giant',
+        },
+      ],
+      imageUrl: '/front-cover-portrait-1.jpg',
+      price: '£4.00',
+      pages: [
+        {
+          title: 'Front Cover',
+          width: 300,
+          height: 600,
+          imageUrl: '/front-cover-portrait-1.jpg',
+        },
+        {
+          title: 'Inside Left',
+          width: 300,
+          height: 600,
+          imageUrl: '',
+        },
+        {
+          title: 'Inside Right',
+          width: 300,
+          height: 600,
+          imageUrl: '',
+        },
+        {
+          title: 'Back Cover',
+          width: 300,
+          height: 600,
+          imageUrl: '/back-cover-portrait.jpg',
+        },
+      ],
+    };
+
+    const card = await cardQuery({ id: 'card001', size: SizeOption.gt });
+    expect(card?.data?.card).toEqual(expectedCard);
+  });
+  it('returns a card with the correct size when size passed as an argument', async () => {
+    const size = SizeOption.sm;
+    const card = await cardQuery({ id: 'card001', size });
+    expect(card?.data?.card.size).toEqual(size);
+  });
+  it('returns a card with a size of null when size is not passed as an argument', async () => {
+    const card = await cardQuery({ id: 'card001' });
+    expect(card?.data?.card.size).toBe(null);
+  });
+  it('returns a card with the correct price when size is passed as an argument', async () => {
+    const card = await cardQuery({ id: 'card002', size: SizeOption.lg });
+    const expectedPrice = '£2.80';
+    expect(card?.data?.card.price).toEqual(expectedPrice);
+  });
+  it('returns a card with the correct price when size is not passed as an argument', async () => {
+    const card = await cardQuery({ id: 'card002' });
+    const expectedPrice = '£2.00';
+    expect(card?.data?.card.price).toEqual(expectedPrice);
+  });
+  it('returns an error when an invalid id is passed as an argument', async () => {
+    const { errors } = await cardQuery({ id: 'someInvalidId' });
+    expect(errors).toHaveLength(1);
+  });
+  it('returns the correct error when an invalid id is passed as an argument', async () => {
+    const { errors } = await cardQuery({ id: 'someInvalidId' });
+    const expectedError = new QueryError(ErrorMessage.cardPreviewGeneration);
+    expect(errors![0]).toEqual(expectedError);
+  });
 });
