9dd48ac005774bfa095bec7a78a3ba58482c56e4
Daniel Barclay <barclaysd@me.com>
2020-11-23 16:12:00 +0000

    Integrate CacheService into NetworkService

--- src/resolvers/CardsResolver.ts
+++ src/resolvers/CardsResolver.ts
@@ -10,12 +10,14 @@ import { Card } from '@/entity/Card';
 import { CardInput } from '@/entity/CardInput';
 import { ISizesResponse } from '@/models/ISizesResponse';
 import { CardService } from '@/services/CardService';
+import { CacheService } from '@/services/CacheService';
 
 @Resolver()
 export default class CardsResolver {
   constructor(
     private networkService: INetwork = new NetworkService(
       process.env.DATABASE_URL!,
+      CacheService.shared,
     ),
   ) {}
 
--- src/services/NetworkService.ts
+++ src/services/NetworkService.ts
@@ -3,14 +3,20 @@ import { INetwork } from '@/models/INetwork';
 import { Endpoint } from '@/models/Endpoints';
 import { QueryError } from '@/entity/QueryError';
 import { ErrorMessage } from '@/models/ErrorMessage';
+import { ICacheService } from '@/models/ICacheService';
 
 export class NetworkService implements INetwork {
-  constructor(public baseURL: string) {}
+  constructor(public baseURL: string, private cacheService: ICacheService) {}
 
   public async get<T>(endpoint: Endpoint): Promise<T> {
     const URL = `${this.baseURL}/${endpoint}.json`;
+    const cacheResultForURL = await this.cacheService.get<T>(URL);
+    if (cacheResultForURL) {
+      return cacheResultForURL;
+    }
     const response = await axios.get(URL);
     if (response.data) {
+      await this.cacheService.set(URL, response.data);
       return response.data;
     }
     throw new QueryError(`${ErrorMessage.generic} for endpoint: ${endpoint}`);
