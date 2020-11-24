c78fbf9d79648090f2e8e8064acea285790955e3
Daniel Barclay <barclaysd@me.com>
2020-11-24 18:20:37 +0000

    Add missing build arg

--- scripts/setupDocker.sh
+++ scripts/setupDocker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker build --build-arg DATABASE_URL="$1" REDIS_URL="$2" -t cards-api-test -f Dockerfile.test .
+docker build --build-arg DATABASE_URL="$1" --build-arg REDIS_URL="$2" -t cards-api-test -f Dockerfile.test .
 docker tag cards-api-test cards-api-test:latest
 docker-compose up -d
 
